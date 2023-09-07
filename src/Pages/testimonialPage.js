1a496bbab430632a0f5de4cdbecc310ace9abfb1
Favour Ukonu <favouroukonu@gmail.com>
2023-09-07 17:26:42 +0100

    testimonial page created

--- src/Pages/testimonialPage.js
+++ src/Pages/testimonialPage.js
@@ -19,7 +19,7 @@ function TestimonialPage() {
           <h3 className="profile-h3 text-left font-medium mb-7">Coordinating event announcements was a<br></br> challenge until we started using the Bulk<br></br> Message Messenger.</h3>
 
           <div className="flex gap-4">
-            <img src={profile2} style={{ width: "50px", height: "50px" }} />
+            <img src={profile2} style={{ width: "50px", height: "50px" }} alt="customer" />
             <div className="text-left">
               <h4 className="profile-h4 text-base font-normal">Elizabeth Alma</h4>
               <p className="text-xs profile-p font-medium">Event Planner</p>
@@ -31,7 +31,7 @@ function TestimonialPage() {
           <h3 className="profile-h3 text-left font-medium mb-7">We needed a solution to manage our large<br></br> scale email communication and Quapaay<br></br> meet our expectations.</h3>
 
           <div className="flex gap-4">
-            <img src={profile1} style={{ width: "50px", height: "50px" }} />
+            <img src={profile1} style={{ width: "50px", height: "50px" }} alt="customer" />
             <div className="text-left">
               <h4 className="profile-h4 text-base font-normal">Mark Anderson</h4>
               <p className="text-xs profile-p font-medium">CEO TechWave Inc.</p>
@@ -42,7 +42,7 @@ function TestimonialPage() {
       </div>
 
       <div className="arrow-icon mt-11 absolute right-16 bottom-16 cursor-pointer">
-        <img src={arrow} style={{ height: "44px", width: "44px" }} />
+        <img src={arrow} style={{ height: "44px", width: "44px" }} alt="arrow-icon" />
       </div>
 
     </section>
