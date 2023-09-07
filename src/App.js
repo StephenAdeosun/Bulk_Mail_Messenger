691385b034afd8bd485399c4f8963aa91ae03f1e
Favour Ukonu <favouroukonu@gmail.com>
2023-09-07 16:50:39 +0100

    testimonial-page created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Section2 from "./Pages/section2";
 import GetStarted from "./Pages/getStarted";
 import Footer from "./Pages/footer";
 import "./App.css";
+import TestimonialPage from "./Pages/testimonialPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <HeroPage />
       <Section2 />
       <GetStarted />
+      <TestimonialPage />
       <Footer />
     </div>
   );
