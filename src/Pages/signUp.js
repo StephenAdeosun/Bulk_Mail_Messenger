93c751b002904bdec860286ee4602ec35c358f37
mishael-codes <enyimishael22@gmail.com>
2023-10-13 15:09:40 +0100

    design bug fixes on the signup page

--- src/Pages/signUp.js
+++ src/Pages/signUp.js
@@ -6,9 +6,9 @@ import "../form.css"
 function Signup() {
   return (
     <>
-      <div className="flex w-screen h-screen bg-white overflow-hidden mx-11 signup">
+      <div className="flex w-screen h-screen bg-white overflow-hidden px-6 signup">
         <IntroductoryPage />
-        <div className="w-1/2 h-full flex justify-center flex-col">
+        <div className="w-full sm:w-1/2 h-full flex justify-center flex-col">
           <div className="lg:w-80 lg:ml-12 md:w-72 md:h-auto md:ml-7">
             <div className="text-left">
               <h2 className="text-black font-bold leading-[40px] text-3xl">
